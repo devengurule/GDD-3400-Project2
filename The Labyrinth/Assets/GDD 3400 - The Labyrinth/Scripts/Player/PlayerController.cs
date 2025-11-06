6e2dff2dbacba20e101f812419e905dd26039b3a
devengurule <dgurule3@uccs.edu>
2025-11-06 13:32:10 -0700

    Got fpc working

--- The Labyrinth/Assets/GDD 3400 - The Labyrinth/Scripts/Player/PlayerController.cs
+++ The Labyrinth/Assets/GDD 3400 - The Labyrinth/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,73 +10,86 @@ namespace  GDD3400.Labyrinth
     public class PlayerController : MonoBehaviour
     {
         [Header("Player Settings")]
-        [SerializeField] private float _MoveSpeed = 10;
-        [SerializeField] private float _DashDistance = 2.5f;
-        [SerializeField] private float _DashCooldown = 1.5f;
+        [SerializeField] private float moveSpeed = 10;
+        [SerializeField] private float dashDistance = 2.5f;
+        [SerializeField] private float dashCooldown = 1.5f;
 
-        [Header("Connections")]
-        [SerializeField] private Transform _GraphicsRoot;
-        private Rigidbody _rigidbody;
-        private InputAction _moveAction;
-        private InputAction _dashAction;
-        private Vector3 _moveVector;
+        [Header("Camera Settings")]
+        [SerializeField] private Camera camera;
+        [SerializeField] private float fov;
+        [SerializeField] private float sensitivity;
+        private float yaw;
+        private float pitch;
 
-        private bool _performDash;
-        private bool _isDashing;
+        private Rigidbody rigidbody;
+        private InputAction moveAction;
+        private InputAction dashAction;
+        private Vector3 inputVector;
+        private Vector3 moveVector;
+
+        private bool performDash;
+        private bool isDashing;
 
         private void Awake()
         {
             // Assign member variables
-            _rigidbody = GetComponent<Rigidbody>();
+            rigidbody = GetComponent<Rigidbody>();
 
-            _moveAction = InputSystem.actions.FindAction("Move");
-            _dashAction = InputSystem.actions.FindAction("Dash");
+            moveAction = InputSystem.actions.FindAction("Move");
+            dashAction = InputSystem.actions.FindAction("Dash");
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
 
         private void Update()
         {
-            // Store current input move vector, remap from X/Y to X/Z
-            _moveVector.x = _moveAction.ReadValue<Vector2>().x;
-            _moveVector.z = _moveAction.ReadValue<Vector2>().y;
+            yaw += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime * 100;
+            pitch -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * 100;
+            pitch = Mathf.Clamp(pitch, -90, 90);
 
-            // Face the player toward the movement direction
-            if (_moveVector.magnitude > 0f) transform.forward = Vector3.Lerp(transform.forward, _moveVector, 0.2f);
+            transform.localRotation = Quaternion.Euler(0f, yaw, 0f);
+            camera.transform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+
+            // Store current input move vector, remap from X/Y to X/Z
+            inputVector.x = moveAction.ReadValue<Vector2>().x;
+            inputVector.z = moveAction.ReadValue<Vector2>().y;
+
+            moveVector = (transform.forward * inputVector.z) + (transform.right * inputVector.x);
+            moveVector.y = 0f;
+            moveVector.Normalize();
+
 
             // If the dash is available and pressed this frame, perform the dash
-            //if (!_isDashing && _dashAction.WasPressedThisFrame()) PerformDash();
+            if (!isDashing && dashAction.WasPressedThisFrame()) PerformDash();
         }
 
         private void FixedUpdate()
         {
             // Apply the movement force
-            _rigidbody.AddForce(_moveVector * _MoveSpeed * 4f, ForceMode.Force);
+            rigidbody.AddForce(moveVector * moveSpeed * 4f, ForceMode.Force);
 
             // Add the dash force
-            if (_performDash)
+            if (performDash)
             {
-                _rigidbody.AddForce(transform.forward * _DashDistance * 5f, ForceMode.Impulse);
-                _performDash = false;
+                rigidbody.AddForce(transform.forward * dashDistance * 5f, ForceMode.Impulse);
+                performDash = false;
             }
+
         }
 
         private void PerformDash()
         {
-            _performDash = true;
-            _isDashing = true;
+            performDash = true;
+            isDashing = true;
 
             // Call reset after the cooldown
-            Invoke("ResetDash", _DashCooldown);
-        }
-
-        private void IsDashing()
-        {
-            // Make invulnurable when dashing
+            Invoke("ResetDash", dashCooldown);
         }
 
         private void ResetDash()
         {
-            _isDashing = false;
-            _rigidbody.linearVelocity = _moveVector * _MoveSpeed;
+            isDashing = false;
+            rigidbody.linearVelocity = moveVector * moveSpeed;
         }
     }
 }
